a74cf1f2841b827657d738b39c6e9d768bca9ea5
Alexander Plavin <alexander@plav.in>
2025-01-01 00:00:00 +0000

    cleaner Makie tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -92,26 +92,26 @@ end
     using Makie.IntervalSets
     using Unitful
 
-    KA = KeyedArray([1 2 3; 4 5 6], a=-10:10:0, b=1:3)
-    KAu = KeyedArray([1 2 3; 4 5 6]u"m", a=(-10:10:0)u"s", b=(1:3)u"W")
-    KA_2d = tuple.(KA, KA)
+    KA2s = [
+        KeyedArray([1 2 3; 4 5 6], a=-10:10:0, b=1:3),
+        KeyedArray([1 2 3; 4 5 6]u"m", a=(-10:10:0)u"s", b=(1:3)u"W"),
+    ]
+    KA_2d = tuple.(KA2s[1], KA2s[1])
 
     fig = Figure()
     @testset for plotf in (:heatmap, :image, :contour, :contourf, :contour3d, :surface)
          # :wireframe  # does it work?
         plotf_excl = Symbol(plotf, :!)
 
-        @eval $plotf(KA)
-        @eval $plotf(fig[1,end+1], KA)
-        @eval $plotf_excl(KA)
+        @testset for KA in KA2s
+            @eval $plotf($KA)
+            @eval $plotf(fig[1,end+1], $KA)
+            @eval $plotf_excl($KA)
 
-        @eval $plotf(KAu)
-        @eval $plotf(fig[1,end+1], KAu)
-        @eval $plotf_excl(KAu)
-
-        @eval $plotf(Observable(KA))
-        @eval $plotf(fig[1,end+1], Observable(KA))
-        @eval $plotf_excl(Observable(KA))
+            @eval $plotf(Observable($KA))
+            @eval $plotf(fig[1,end+1], Observable($KA))
+            @eval $plotf_excl(Observable($KA))
+        end
     end
 
     @testset for plotf in (:arrows,)
@@ -135,20 +135,26 @@ end
         @eval $plotf_excl(KA_nonunif)
     end
 
-    KA1d = KeyedArray([1, 5, 2, 1], x=[1, 5, 10, 20]u"km")
+    KA1ds = [
+        KeyedArray([1, 5, 2, 1]u"W", x=[1, 5, 10, 20]u"km"),
+        KeyedArray([1, 5, 2, 1], x=[1, 5, 10, 20]u"km"),
+        KeyedArray([1, 5, 2, 1], x=[1, 5, 10, 20]),
+    ]
     @testset for plotf in (:scatter, :lines, :scatterlines, :stairs, :stem, :barplot)
         plotf_excl = Symbol(plotf, :!)
 
-        @eval $plotf(KA1d)
-        @eval $plotf(fig[1,end+1], KA1d)
-        @eval $plotf_excl(KA1d)
+        @testset for KA1d in KA1ds
+            @eval $plotf($KA1d)
+            @eval $plotf(fig[1,end+1], $KA1d)
+            @eval $plotf_excl($KA1d)
 
-        @eval $plotf(Observable(KA1d))
-        @eval $plotf(fig[1,end+1], Observable(KA1d))
-        @eval $plotf_excl(Observable(KA1d))
+            @eval $plotf(Observable($KA1d))
+            @eval $plotf(fig[1,end+1], Observable($KA1d))
+            @eval $plotf_excl(Observable($KA1d))
+        end
     end
-    KA1d = KeyedArray(Interval.([1, 5, 2, 1], [1, 5, 2, 1] .+ 1), x=[1, 5, 10, 20]u"km")
 
+    KA1d = KeyedArray(Interval.([1, 5, 2, 1], [1, 5, 2, 1] .+ 1), x=[1, 5, 10, 20]u"km")
     @testset for plotf in (:band, :rangebars)
         plotf_excl = Symbol(plotf, :!)
 
@@ -161,17 +167,22 @@ end
         @eval $plotf_excl(Observable(KA1d))
     end
 
-    KA3d = KeyedArray(reshape(1:12, (2, 3, 2)), a=-10:10:0, b=1:3, c=[5, 7]u"km")
+    KA3ds = [
+        KeyedArray(reshape(1:12, (2, 3, 2)), a=-10:10:0, b=1:3, c=[5, 7]u"km"),
+        KeyedArray(reshape(1:12, (2, 3, 2)), a=-10:10:0, b=1:3, c=[5, 7]),
+    ]
     @testset for plotf in (:volume, :volumeslices)
         plotf_excl = Symbol(plotf, :!)
 
-        @eval $plotf(KA3d)
-        @eval $plotf(fig[1,end+1], KA3d)
-        @eval $plotf_excl(KA3d)
+        @testset for KA3d in KA3ds
+            @eval $plotf($KA3d)
+            @eval $plotf(fig[1,end+1], $KA3d)
+            @eval $plotf_excl($KA3d)
 
-        @eval $plotf(Observable(KA3d))
-        @eval $plotf(fig[1,end+1], Observable(KA3d))
-        @eval $plotf_excl(Observable(KA3d))
+            @eval $plotf(Observable($KA3d))
+            @eval $plotf(fig[1,end+1], Observable($KA3d))
+            @eval $plotf_excl(Observable($KA3d))
+        end
     end
 end
 
