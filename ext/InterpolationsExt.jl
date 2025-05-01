3839548a68adb654c5d4732513c8aebfc5ccbeef
Alexander Plavin <alexander@plav.in>
2025-05-01 00:00:00 +0000

    import interpolations support: irregular and decreasing axiskeys

--- ext/InterpolationsExt.jl
+++ ext/InterpolationsExt.jl
@@ -11,9 +11,33 @@ end
 KeyedInterpolation(dimnames, interpolation) = KeyedInterpolation{dimnames}(interpolation)
 KeyedInterpolation{dimnames}(interpolation) where {dimnames} = KeyedInterpolation{dimnames, typeof(interpolation)}(interpolation)
 
-Interpolations.interpolate(A::KeyedArray, it::Interpolations.DimSpec{BSpline}) = KeyedInterpolation(dimnames(A), scale(interpolate(parent(A), it), axiskeys(A)...))
+function Interpolations.linear_interpolation(A::KeyedArray; extrapolation_bc=Throw())
+    it = all(ak -> ak isa AbstractRange, axiskeys(A)) ? BSpline(Linear()) : Gridded(Linear())
+    extrapolate(interpolate(A, it), extrapolation_bc)
+end
+
+function Interpolations.interpolate(A::KeyedArray, it::Interpolations.DimSpec{BSpline})
+    all(ak -> ak isa AbstractRange, axiskeys(A)) || throw(ArgumentError("KeyedArray with BSpline interpolation requires all axiskeys to be AbstractRanges. Use Gridded interpolation instead."))
+    if all(ak -> step(ak) > zero(step(ak)), axiskeys(A))
+        KeyedInterpolation(dimnames(A), scale(interpolate(parent(A), it), axiskeys(A)...))
+    else
+        dim_ixs = findall(ak -> step(ak) < zero(step(ak)), axiskeys(A)) |> Tuple
+        Ar = reverse(A; dims=dim_ixs)
+        interpolate(Ar, it)
+    end
+end
+
+function Interpolations.interpolate(A::KeyedArray, it::Interpolations.DimSpec{Gridded})
+    if all(issorted, axiskeys(A))
+        KeyedInterpolation(dimnames(A), interpolate(axiskeys(A), parent(A), it))
+    else
+        dim_ixs = findall(ak -> issorted(ak, rev=true), axiskeys(A)) |> Tuple
+        Ar = reverse(A; dims=dim_ixs)
+        KeyedInterpolation(dimnames(A), interpolate(axiskeys(Ar), parent(Ar), it))
+    end
+end
+
 Interpolations.extrapolate(A::KeyedInterpolation, et) = KeyedInterpolation(dimnames(A), extrapolate(A.interpolation, et))
-Interpolations.linear_interpolation(A::KeyedArray; extrapolation_bc=Throw()) = extrapolate(interpolate(A, BSpline(Linear())), extrapolation_bc)
 
 (ki::KeyedInterpolation)(args::Vararg{Number}) = ki.interpolation(args...)
 (ki::KeyedInterpolation)(args::NTuple{<:Any, Number}) = ki.interpolation(args...)
